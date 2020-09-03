fbb05c09c0dbe92ff9e1b21b6912b2ddb976960c
khushi-k-shah <54678506+khushi-k-shah@users.noreply.github.com>
2020-09-03 00:34:16 -0700

    Update Airline.java

--- AirportManager/src/Airline.java
+++ AirportManager/src/Airline.java
@@ -1,12 +1,5 @@
 import java.io.Serializable;
 
-/**
- * Project 5 - Airport Manager
- *
- * @author Khushi Shah, Natalie Renee Evoniuk, Lab section g20
- * @version 12/3/19
- */
-
 public interface Airline extends Serializable {
     public String getAirline();
 }
